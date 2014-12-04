1046ee60ad95e62daca0c2c4f1ead8663d087b7b
Javier Hernández <jhernandez@emergya.com>
2014-12-04 17:41:32 +0100

    Initial concept of the implementation

---
+++ index.js
@@ -0,0 +1,59 @@
+var app = require('express')();
+var bp = require('body-parser');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var crypto = require("crypto");
+var util = require("util");
+
+var clients = {};
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/listClients', function(req, res){
+  var c = "";
+  for(var id in clients){
+    c = c + "<p>" + id + "</p>";
+  }
+  res.send("" + c);
+});
+
+var parser = bp.json();
+
+app.post('/getSettings', parser, function(req, res) {
+  console.log("### params: " + JSON.stringify(req.body));
+  clients[req.body.id].emit('get settings', req.body.settings);
+  res.send("ok");
+});
+
+app.post('/setSettings', parser, function (req, res) {
+  console.log("### params: " + JSON.stringify(req.body));
+  clients[req.body.id].emit('set settings', req.body.settings);
+  res.send("ok");
+});
+
+io.on('connection', function(socket){
+  clients[socket.id] = socket;
+  socket.emit('browser registered', socket.id);
+  console.log('## a new browser has been registered with id: ' + socket.id);
+
+  socket.on('got settings', function(settings) {
+    console.log("## settings retrieved for browser id: " + socket.id);
+    console.log("## settings: " + JSON.stringify(settings, null, 2));
+  });
+
+  socket.on('settings applied', function (returnPayload) {
+    console.log("## settings appied in browser with id: " + socket.id);
+    console.log("## response is: " + JSON.stringify(returnPayload, null, 4));
+  });
+
+  socket.on('disconnect', function(){
+    console.log('## a browser has disconnected with id: ' + socket.id);
+    delete(clients[socket.id]);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
