242f1ec22cb50a12a58fd60046f9a2d54fe483ec
Javier Hernández <jhernandez@emergya.com>
2014-12-05 19:31:25 +0100

    Cleaning up a bit

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var app = require('express')();
 var bp = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var crypto = require("crypto");
-var util = require("util");
 
 var clients = {};
 
@@ -39,12 +37,12 @@ io.on('connection', function(socket){
   console.log('## a new browser has been registered with id: ' + socket.id);
 
   socket.on('got settings', function(settings) {
-    console.log("## settings retrieved for browser id: " + socket.id);
-    console.log("## settings: " + JSON.stringify(settings, null, 2));
+    console.log("## settings retrieved from browser with id: " + socket.id);
+    console.log("## settings: " + JSON.stringify(settings, null, 4));
   });
 
   socket.on('settings applied', function (returnPayload) {
-    console.log("## settings appied in browser with id: " + socket.id);
+    console.log("## settings applied in browser with id: " + socket.id);
     console.log("## response is: " + JSON.stringify(returnPayload, null, 4));
   });
 
